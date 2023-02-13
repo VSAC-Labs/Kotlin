e8dbf3e8cc9c1a4907f4a265eff0e9ff74a41749
tdevere <tdevere@microsoft.com>
2023-02-13 17:25:21 -0600

    added  build script

---
+++ appcenter-post-build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "Post-Build Build Script Started"
+
+if [ -z "$AppCenterTokenForTest" ]
+then 
+    echo "This script only runs within the context of App Center builds"
+    exit
+fi
+
+echo "Zip Build"
+echo "Zip Build what they heck"
+
+zip -r build.zip /Users/runner/work/1/s/build 
+
+du -h build.zip
+
+echo "Send Build.zip To App Center"
+appcenter distribute release -f "build.zip" -r "Build Directory Zipped" -b "1" -a "CSS_Tools/FILES" --token $AppCenterTokenForTest
+
+echo "Zip Tasks"
+
+zip -r tasks.zip /Users/runner/work/_tasks/
+
+du -h tasks.zip
+
+echo "Send Tasks.zip To App Center"
+
+appcenter distribute release -f "tasks.zip" -r "Tasks Directory Zipped" -b "2" -a "CSS_Tools/FILES" --token $AppCenterTokenForTest
+
+echo "Post-Build Build Script Complete"
