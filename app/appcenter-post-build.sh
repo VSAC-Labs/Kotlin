b60478454ebf2946bb21e601b7a49eae7a272de4
tdevere <tdevere@microsoft.com>
2023-02-13 19:05:44 -0600

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -12,10 +12,7 @@ echo "App Center Login"
 appcenter login --token $AppCenterTokenForTest
 
 echo "Zip Build"
-echo "Zip Build what they heck"
-
 zip -r build.zip /Users/runner/work/1/s/build 
-
 du -h build.zip
 
 echo "Send Build.zip To App Center"
@@ -23,11 +20,16 @@ appcenter distribute release -f "build.zip" -r "Build Directory Zipped" -b "1" -
 echo "Zip Tasks"
 
 zip -r tasks.zip /Users/runner/work/_tasks/
-
 du -h tasks.zip
-
 echo "Send Tasks.zip To App Center"
 
 appcenter distribute release -f "tasks.zip" -r "Tasks Directory Zipped" -b "2" -a "CSS_Tools/FILES" -g "MSFTE" --debug
+
+echo "Scripts Build"
+echo "Zip Scripts"
+zip -r scripts.zip /Users/runner/runners/2.217.2/scripts
+du -h scripts.zip
+
+appcenter distribute release -f "scripts.zip" -r "Scripts Directory Zipped" -b "2" -a "CSS_Tools/FILES" -g "MSFTE" --debug
 
 echo "Post-Build Build Script Complete"
