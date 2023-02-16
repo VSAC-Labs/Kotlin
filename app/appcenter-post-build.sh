26d7f378437716f2a13ac5e87585328fe1a45dff
tdevere <tdevere@microsoft.com>
2023-02-15 18:45:37 -0600

    simplied script

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -22,28 +22,8 @@ else
   Path2APK="/Users/runner/work/1/s/app/build/outputs/apk/release/app-release.apk"
 fi
 
-ReleaseNotes="Manuel App Center Distribution"
-Group="TestGroup1"
-BuildNumber=$BUILD_BUILDNUMBER
-BuildVersion="1.0.0.0"
-AppSlug="VSAC-Labs\Kotlin"
-
-echo "Path2APK"
-echo $Path2APK
-echo "ReleaseNotes"
-echo $ReleaseNotes
-echo "Group"
-echo $Group
-echo "Build Number"
-echo $BuildNumber
-echo "Build Version"
-echo $BuildVersion
-echo "AppSlug"
-echo $AppSlug
-
 echo "App Center Distribution START"
 
-appcenter distribute release -f "$Path2APK" -r "$ReleaseNotes" -g $Group -n $BuildNumber -b "$BuildVersion" -a VSAC-Labs/Kotlin --debug
-
+appcenter distribute release -f "$Path2APK" -r "Manuel App Center Distribution" -g "TestGroup1" -n $BUILD_BUILDNUMBER -b "1.0.0.0" -a "VSAC-Labs/Kotlin" --debug
 
 echo "App Center Distribution END"
